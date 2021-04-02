5196062cdee80ce12367f40b3283a873a322a275
Thang Cao <shiyoline@gmail.com>
2021-04-01 21:01:05 -0700

    Add login GUI

--- src/application/Main.java
+++ src/application/Main.java
@@ -12,9 +12,9 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			Parent root = FXMLLoader.load(getClass().getResource("test.fxml"));
+			Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
 
-			Scene scene = new Scene(root,400,400);
+			Scene scene = new Scene(root,1000,700);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
