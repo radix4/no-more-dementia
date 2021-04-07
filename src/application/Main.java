c561cf763f4feeaf81cf3480f95f27b9e06d2b8a
Thang Cao <shiyoline@gmail.com>
2021-04-07 14:53:01 -0700

    Refactor UI files into own package, update UI elements IDs

--- src/application/Main.java
+++ src/application/Main.java
@@ -5,14 +5,13 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			Parent root = FXMLLoader.load(getClass().getResource("registration.fxml"));
+			Parent root = FXMLLoader.load(getClass().getResource("layout/login.fxml"));
 
 			Scene scene = new Scene(root,1000,700);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
